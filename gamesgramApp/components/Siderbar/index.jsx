91f9a1056c687f2d089812a2889ecf3a87eea471
Wilson Zheng <zhengjp600@outlook.com>
2023-06-03 18:33:25 +0200

    Auto fetch userInfo in context

--- gamesgramApp/components/Siderbar/index.jsx
+++ gamesgramApp/components/Siderbar/index.jsx
@@ -4,11 +4,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect, useContext } from "react";
 import Context from "@/context/Context";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 //Component for the platforms sidebar
 export default function Sidebar(props) {
-
   //Hooks for css states of each sidebar component
   const { userInfo, setuserInfo } = useContext(Context);
   const [steamid, setSteamid] = useState();
@@ -17,30 +16,8 @@ export default function Sidebar(props) {
   //react router variable
   const router = useRouter();
 
-  //TODO DIFFERENT WAY OF MAKEING SURE CONTEXT IS IGVEN?
-  const GetuserInfo = async () => {
-    if (!userInfo && window.localStorage.getItem("token")) {
-      // API endpoint where we send form data.
-      const endpoint = "/api/GetUserInfo";
-      // Form the request for sending data to the server.
-      const options = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          token: window.localStorage.getItem("token"),
-        },
-      };
-    
-      // Send the form data to our forms API on Vercel and get a response.
-      const response = await fetch(endpoint, options);
-      const data = await response.json();
-      setuserInfo(data.response.players[0]);
-    }
-  };
-
   //TODO: maybe google if there is a better way
   useEffect(() => {
-    GetuserInfo();
     if (userInfo && !steamid) {
       initFields();
     }
@@ -50,7 +27,6 @@ export default function Sidebar(props) {
     setSteamid(userInfo.steamid);
     setAvatar(userInfo.avatar);
   };
-
 
   //Icon header of the sidebar
   const Header = () => {
@@ -71,83 +47,120 @@ export default function Sidebar(props) {
     );
   };
 
-    //Function for user signout. Deletes local storage and token from database
-    const handleSignout = async ()=>{
+  //Function for user signout. Deletes local storage and token from database
+  const handleSignout = async () => {
+    const endpoint = "/api/SignOut";
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        token: window.localStorage.getItem("token"),
+      },
+    };
+    const response = await fetch(endpoint, options);
 
-      const endpoint = '/api/SignOut'
-            const options = {
-                method: 'DELETE',
-                headers: {
-                'Content-Type': 'application/json',
-                'token':window.localStorage.getItem("token"),
-                },
-            }
-            const response = await fetch(endpoint, options)
-            
-            if (response.status===200){
-              localStorage.clear();
-              window.localStorage.clear();
-            }
-            if (typeof window !== "undefined") {router.push('/')}
+    if (response.status === 200) {
+      localStorage.clear();
+      window.localStorage.clear();
+    }
+    if (typeof window !== "undefined") {
+      router.push("/");
+    }
+  };
+
+  //Function for hovering functionality of navigation bar
+  const NavigationBar = () => {
+    //hover over sidebar event changes background color
+    function mouseOver(e) {
+      e.target.style.background = "#06377e";
+    }
+    //once hover has ended, background color changes
+    function hoverLeave(e) {
+      e.target.style.background = "";
     }
 
-//Function for hovering functionality of navigation bar
-    const NavigationBar = () => {
-
-      //hover over sidebar event changes background color
-      function mouseOver(e){
-        e.target.style.background = '#06377e';
-      }
-      //once hover has ended, background color changes
-      function hoverLeave(e) {
-        e.target.style.background = '';
-      }
-      
-      return (
-        <div >
-          <ul role="button" className="nav nav-pills flex-column mb-auto">
-            <li className="nav-item">
-              
-              <Link href="/" id="Home" className={props.selection.home} onMouseOver={mouseOver} onMouseLeave={hoverLeave}>
-                Home
-              </Link>
-            </li>
-            <li>
-                <Link href="/Search" id="Search" className={props.selection.search} onMouseOver={mouseOver} onMouseLeave={hoverLeave} >
-                  Search
-                </Link>
-            </li>
-            <li>
-              <a id="Reels" className={props.selection.reels} onMouseOver={mouseOver} onMouseLeave={hoverLeave}>
-                Galaries/Reels
-              </a>
-            </li>
-            <li>
-              <Link href={`/teamMates/?steamid=${steamid}`} as={"/teamMates"} id="TeamMates" className={props.selection.teamMates} onMouseOver={mouseOver} onMouseLeave={hoverLeave}>
-                TeamMates
-              </Link>
-            </li>
-            <li>
-              <Link href={`/${steamid}`} as={`/${steamid}`} id="Profile" className={props.selection.profile} onMouseOver={mouseOver} onMouseLeave={hoverLeave}>
-                {avatar ? <Image 
-                src={avatar}
-                width={20}
-                height={20}
-                alt={steamid}
-                className={styles.avatar}
-                />: null}
-                 Profile
-              </Link>
-              <hr/>
-              <a id="Signout" className={props.selection.signout}  onClick={handleSignout} onMouseOver={mouseOver} onMouseLeave={hoverLeave}>
-                SignOut
-              </a>
-            </li>
-          </ul>
-        </div>
-      );
-
-    };
+    return (
+      <div>
+        <ul role="button" className="nav nav-pills flex-column mb-auto">
+          <li className="nav-item">
+            <Link
+              href="/"
+              id="Home"
+              className={props.selection.home}
+              onMouseOver={mouseOver}
+              onMouseLeave={hoverLeave}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/Search"
+              id="Search"
+              className={props.selection.search}
+              onMouseOver={mouseOver}
+              onMouseLeave={hoverLeave}
+            >
+              Search
+            </Link>
+          </li>
+          <li>
+            <a
+              id="Reels"
+              className={props.selection.reels}
+              onMouseOver={mouseOver}
+              onMouseLeave={hoverLeave}
+            >
+              Galaries/Reels
+            </a>
+          </li>
+          <li>
+            <Link
+              href={`/teamMates/?steamid=${steamid}`}
+              as={"/teamMates"}
+              id="TeamMates"
+              className={props.selection.teamMates}
+              onMouseOver={mouseOver}
+              onMouseLeave={hoverLeave}
+            >
+              TeamMates
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={`/${steamid}`}
+              as={`/${steamid}`}
+              id="Profile"
+              className={props.selection.profile}
+              onMouseOver={mouseOver}
+              onMouseLeave={hoverLeave}
+            >
+              {avatar ? (
+                <Image
+                  src={avatar}
+                  width={20}
+                  height={20}
+                  alt={steamid}
+                  className={styles.avatar}
+                />
+              ) : null}
+              Profile
+            </Link>
+            <hr />
+            <a
+              id="Signout"
+              className={props.selection.signout}
+              onClick={handleSignout}
+              onMouseOver={mouseOver}
+              onMouseLeave={hoverLeave}
+            >
+              SignOut
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  };
 
   //return of the sidebar
   return (
@@ -164,5 +177,4 @@ export default function Sidebar(props) {
       </div>
     </>
   );
-
 }
--- gamesgramApp/context/Context.js
+++ gamesgramApp/context/Context.js
@@ -1,14 +1,36 @@
-import { createContext, useState } from 'react';
+import { createContext, use, useEffect, useState } from "react";
 
 const Context = createContext();
 
-
 //Context for information about the logged in user (to provide it to every component)
 export const Provider = ({ children }) => {
-  const [userInfo,setuserInfo] = useState(null);
+  const [userInfo, setuserInfo] = useState(null);
+  const GetuserInfo = async () => {
+    if (!userInfo && window.localStorage.getItem("token")) {
+      // API endpoint where we send form data.
+      const endpoint = "/api/GetUserInfo";
+      // Form the request for sending data to the server.
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          token: window.localStorage.getItem("token"),
+        },
+      };
 
+      // Send the form data to our forms API on Vercel and get a response.
+      const response = await fetch(endpoint, options);
+      const data = await response.json();
+      setuserInfo(data.response.players[0]);
+    }
+  };
+  useEffect(() => {
+    if (!userInfo) {
+      GetuserInfo();
+    }
+  }, [userInfo]);
   return (
-    <Context.Provider value={{ userInfo,setuserInfo }}>
+    <Context.Provider value={{ userInfo, setuserInfo }}>
       {children}
     </Context.Provider>
   );
