74e7cf344d4f38b027f2b14f95445f0379d28f24
Wilson Zheng <zhengjp600@outlook.com>
2023-06-03 17:32:01 +0200

    implement auto reload after create new Post

--- gamesgramApp/pages/[steamid].jsx
+++ gamesgramApp/pages/[steamid].jsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState, useContext } from "react";
-import Head from 'next/head';
+import Head from "next/head";
 import Sidebar from "@/components/Siderbar";
 import Profile from "@/components/Profile";
 import styles from "@/styles/Home.module.css";
@@ -16,29 +16,52 @@ const UserProfile = ({
   userFollower,
   userFriends,
   userPosts,
+  steamid,
 }) => {
   //Data hooks for user profile information
   const [userViewInfo, setUserInfo] = useState(dataUserInfo); //Information about inspected user
   const [follower, setFollower] = useState(userFollower); //Follower of inspected user
   const [friends, setFriends] = useState(userFriends); //Firends of inspected user
   const [mediaPosts, setMediaPosts] = useState(userPosts); //Posts of inspected user
+  const [isReload, setReload] = useState(false); //Reload hook for profile information
 
+  console.log("steamid", steamid);
   //React router variable
   const router = useRouter();
 
+  const handleReload = (data) => {
+    setReload(data);
+  };
+
+  //fetching post from server
+  const fetchPost = async () => {
+    const resposts = await fetch(`http://127.0.0.1:5001/getPosts/${steamid}`);
+    const posts = await resposts.json();
+    setMediaPosts(posts);
+  };
+
+  //Use effect for reloading post
+  useEffect(() => {
+    if (isReload) {
+      setReload(false);
+      fetchPost();
+    }
+  }, [isReload]);
+
   //Constant for the sidebar selection highlight which is passed as a prop to child components
-  const SidebarSelect = 
-                        {home: "nav-link text-white",
-                        search: "nav-link text-white",
-                        reels: "nav-link text-white",
-                        teamMates: "nav-link text-white",
-                        profile: "nav-link active",
-                        signout: "nav-link text-white"};
+  const SidebarSelect = {
+    home: "nav-link text-white",
+    search: "nav-link text-white",
+    reels: "nav-link text-white",
+    teamMates: "nav-link text-white",
+    profile: "nav-link active",
+    signout: "nav-link text-white",
+  };
   return (
     <main>
       <Head>
-          <title>GamesGram Profile</title>
-          <link rel="icon" href="/images/GPic.jpg" />
+        <title>GamesGram Profile</title>
+        <link rel="icon" href="/images/GPic.jpg" />
       </Head>
       <div>
         <Modal
@@ -62,14 +85,28 @@ const UserProfile = ({
         </Modal>
       </div>
 
-    <div className={styles.main}>
-        <div className={styles.one}><Sidebar selection={SidebarSelect}/></div>
-        <div><Profile userInfo={userViewInfo[0]} follower={follower} friends={friends}/></div>
-        <div className={styles.two}>
-          {mediaPosts&&userViewInfo ? <PostWall userInfo={userViewInfo[0]} media={mediaPosts} />: "No posts available"}:
+      <div className={styles.main}>
+        <div className={styles.one}>
+          <Sidebar selection={SidebarSelect} />
         </div>
-    </div>
-</main>
+        <div>
+          <Profile
+            userInfo={userViewInfo[0]}
+            follower={follower}
+            friends={friends}
+            handleReload={handleReload}
+          />
+        </div>
+        <div className={styles.two}>
+          {mediaPosts && userViewInfo ? (
+            <PostWall userInfo={userViewInfo[0]} media={mediaPosts} />
+          ) : (
+            "No posts available"
+          )}
+          :
+        </div>
+      </div>
+    </main>
   );
 };
 
@@ -118,6 +155,7 @@ export async function getStaticProps({ params }) {
       userFollower: follower,
       userFriends: friends,
       userPosts: posts,
+      steamid: params.steamid,
     },
   };
 }
